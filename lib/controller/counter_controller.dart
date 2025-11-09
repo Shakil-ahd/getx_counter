bebaa8eef82528cf5f22aecbd5063246aebf82ac
Shakil Ahmed <shakilahmed.of@gmail.com>
2025-11-09 22:53:14 +0600

    aa

--- lib/controller/counter_controller.dart
+++ lib/controller/counter_controller.dart
@@ -3,11 +3,7 @@ import 'package:get/get.dart';
 class CounterController extends GetxController {
   var count = 0.obs;
 
-  void increment() {
-    count++;
-  }
+  void increment() => count++;
 
-  void decrement() {
-    count--;
-  }
+  void decrement() => count--;
 }
--- lib/counter_page.dart
+++ lib/counter_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_counter/routes/controller/counter_controller.dart';
+import 'package:getx_counter/controller/counter_controller.dart';
 
 class CounterPage extends StatelessWidget {
   final CounterController controller = Get.put(
